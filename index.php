da441b80ff26c733530fc04bbf7db1f24ff43f0c
jsmitsl <163905526+jsmitsl@users.noreply.github.com>
2025-03-23 03:57:53 +0000

    creando rectángulo y ejercicioRectangulo

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <a href="ejercicioRectangulo.php">Ejercicio Rectángulo</a>
+</body>
 </html>
